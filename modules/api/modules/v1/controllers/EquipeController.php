200e8900842a6df2d2fc577c666bbe99b301e274
tiago <tiago.alexandre.oliveira@hotmail.com>
2022-05-12 10:12:41 -0300

    Geração de QrCode

--- modules/api/modules/v1/controllers/EquipeController.php
+++ modules/api/modules/v1/controllers/EquipeController.php
@@ -4,6 +4,8 @@ namespace app\modules\api\modules\v1\controllers;
 
 use app\models\Equipe;
 use app\models\User;
+use Da\QrCode\QrCode;
+use yii\helpers\Url;
 use yii\web\BadRequestHttpException;
 use yii\web\UnauthorizedHttpException;
 
@@ -132,7 +134,15 @@ class EquipeController extends DefaultController
      *         type="string",
      *         required=true,
      *     ),
-     *
+     *     @SWG\Parameter(
+     *         description="equipe",
+     *         in="body",
+     *         name="body",
+     *         required=true,
+     *         @SWG\Schema(
+     *          @SWG\Property(property="equipe_id", type="string"),
+     *       )
+     *     ),
      *     @SWG\Response(
      *         response = 200,
      *         description = "User collection response",
@@ -185,6 +195,7 @@ class EquipeController extends DefaultController
      *         required=true,
      *         @SWG\Schema(
      *          @SWG\Property(property="users", type="array", description="Users_id", @SWG\Items(type="integer")),
+     *          @SWG\Property(property="equipe_id", type="string"),
      *       )
      *     ),
      *     @SWG\Response(
@@ -218,6 +229,69 @@ class EquipeController extends DefaultController
         }
         $equipe->removeParticipante($this->_post["users"]);
         return true;
+    }
+
+    /**
+     * @SWG\Post(path="/api/v1/equipe/get-qrcode",
+     *     tags={"Equipe"},
+     *     summary="Retorna o qrcode da equipe para ser pontuada",
+     *     @SWG\Parameter(
+     *         description="Token retornado na função de login",
+     *         in="header",
+     *         name="x-token",
+     *         required=true,
+     *         type="string",
+     *         required=true,
+     *     ),
+     *      @SWG\Parameter(
+     *         description="equipe",
+     *         in="body",
+     *         name="body",
+     *         required=true,
+     *         @SWG\Schema(
+     *          @SWG\Property(property="equipe_id", type="string"),
+     *       )
+     *     ),
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "User collection response",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="status", type="integer", description="", default="200"),
+     *              @SWG\Property(property="qr_code", type="string", description="", default="200"),
+     *          ),
+     *     ),
+     *     @SWG\Response(
+     *         response = 400,
+     *         description = "Bad Request",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="name", type="string",description="qual foi o erro, muito provavelmente será Unauthorized"),
+     *              @SWG\Property(property="message", type="string", description=""),
+     *              @SWG\Property(property="code", type="string", description="Esse code é para o desenvolvedor back, nao faz diferença pro front"),
+     *              @SWG\Property(property="status", type="integer", description="mesmo status code http"),
+     *              @SWG\Property(property="type", type="integer", description="Também pro backend, pra saber qual classe que chamou o retorno de falha"),
+     *
+     *          ),
+     *     ),
+     * )
+     * @return mixed
+     * @throws BadRequestHttpException
+     */
+    public function actionGetQrcode(){
+        $equipe = Equipe::findOne(["id" => $this->_post["equipe_id"]]);
+        if(!$equipe){
+            throw new BadRequestHttpException("Equipe não encontrada");
+        }
+        if(!\Yii::$app->cache->get("qrcode_".$equipe->id)) {
+            $qrCode = (new QrCode('http://jogodacidade.com.br/equipe/pontos?id='.$equipe->id))
+                ->setSize(250)
+                ->setMargin(5)
+                ->setForegroundColor(51, 153, 255);
+            $base_64 = $qrCode->writeDataUri();
+            \Yii::$app->cache->set("qrcode_".$equipe->id, $base_64, 9000);
+        }else{
+            $base_64 = \Yii::$app->cache->get("qrcode_".$equipe->id);
+        }
+        return ["base" => $base_64];
     }
 
 }
